1ee32f0a7807650faf056da0fa21444937e560c8
matebook <rwy23@mails.tsinghua.edu.cn>
2025-09-28 18:52:45 +0800

    example

--- R.lean
+++ R.lean
@@ -1,3 +1,10 @@
--- This module serves as the root of the `R` library.
--- Import modules here that should be built as part of the library.
-import R.Basic
+import Mathlib
+
+theorem thm1 (P Q R : Prop) : P → (Q ∧ R) ↔ (P → Q) ∧ (P → R) := by
+  -- P → (Q ∧ R) ↔ ¬P ∨ (Q ∧ R)
+  rw [imp_iff_not_or]
+  -- ¬P ∨ (Q ∧ R) ↔ (¬P ∨ Q) ∧ (¬P ∨ R)
+  rw [or_and_left]
+  -- (¬P ∨ Q) ∧ (¬P ∨ R) ↔ (P → Q) ∧ (P → R)
+  rw [←imp_iff_not_or]
+  rw [←imp_iff_not_or]
